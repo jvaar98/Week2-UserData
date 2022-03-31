ef2676e53abab54d573c9438906d41c1b7594cad
Jaagup Vaar <jaagupvaar@MacBook-Air.lan>
2022-03-31 11:05:13 +0300

    Control Flow

---
+++ UserAge/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UserAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Tere!");
+            //programm küsib kasutaja vanust;
+            //programm arvutab kasutaja sünniaastat;
+            //programm kuvab sünniaastat konsoolis;
+            //"oled sündinud aastal {yearOfBirth}"
+
+            Console.WriteLine("Kui vana sa oled?");
+
+            int userAge = Convert.ToInt32(Console.ReadLine());
+
+            int yearOfBirth = 2022 - userAge;
+
+            Console.WriteLine($"Oled sündinud aastal {yearOfBirth}.");
+
+        }
+    }
+}
